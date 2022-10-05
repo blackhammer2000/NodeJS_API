97d4c6695e77a14081bb9738d26df641dc909c33
wawerusamuel <waweruzamuel@gmail.com>
2022-10-05 20:53:07 +0300

    added the dotenv dependency and the nodemon dependency...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { config } from "dotenv";
+config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
