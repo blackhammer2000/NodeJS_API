e940931c07e9d9975590a0c81902cb6af8d5ed48
wawerusamuel <waweruzamuel@gmail.com>
2022-10-06 14:23:01 +0300

    added the mongodb dependency...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-import { router } from "./routes/api";
+import router from "./routes/api";
+
 // import { rootDirectoryGet } from "./routes/api.js";
 config();
 
@@ -12,5 +13,3 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is listening for requests on PORT ${PORT}`);
 });
-
-app.use("/", router);
