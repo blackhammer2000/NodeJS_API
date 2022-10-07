dffc03e6b7fd8561416195d2de08fc88a8a696cb
wawerusamuel <waweruzamuel@gmail.com>
2022-10-07 15:40:00 +0300

    added an admission field in the students schema...

--- schemas/student.js
+++ schemas/student.js
@@ -13,6 +13,10 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: [true, "Gender must be provided"],
   },
+  admission_number: {
+    type: Number,
+    required: [true, "Admission number is required"],
+  },
 });
 
 const Student = mongoose.model("student", studentSchema);
