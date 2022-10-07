61ed73d1188a993d80d402b30cf5aaa69781d701
wawerusamuel <waweruzamuel@gmail.com>
2022-10-07 15:32:30 +0300

    added successfultion to the local mongodb database...

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = require("./router/routes");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const databaseStudentSchema = require("./schemas/student");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DATABASE_URI;
 
 app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is listening for requests on PORT ${PORT}`);
+});
+
+mongoose.connect(DB_URI, () => {
+  console.log("connected to the database...");
 });
