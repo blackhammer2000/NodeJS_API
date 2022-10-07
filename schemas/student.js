383bdc84473c3b7a0bfc336b387bf3a65538edb4
wawerusamuel <waweruzamuel@gmail.com>
2022-10-07 15:09:47 +0300

    created and exported the student schema...

--- schemas/student.js
+++ schemas/student.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema();
+const studentSchema = new mongoose.Schema({
+  firstname: {
+    type: string,
+    required: true,
+  },
+  lastname: {
+    type: string,
+    required: true,
+  },
+  gender: {
+    type: string,
+    required: true,
+  },
+});
+
+const Student = mongoose.model("student", studentSchema);
+
+module.exports = Student;
