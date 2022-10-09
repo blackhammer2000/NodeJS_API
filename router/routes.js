26569994bcdb0384a8b10156e085543e6d43095e
wawerusamuel <waweruzamuel@gmail.com>
2022-10-09 20:24:48 +0300

    updated the variable names in the delete route...

--- router/routes.js
+++ router/routes.js
@@ -49,7 +49,7 @@ router.patch("/students", async (req, res) => {
         throw new Error("Could not update the selected document.");
       }
 
-      res.json({ message: "Data successfully modified..." });
+      res.json({ message: "Data successfully modified in the database..." });
     } else {
       res.status(404).json({ error: "Not a valid document ID." });
     }
@@ -63,15 +63,15 @@ router.delete("/students", async (req, res) => {
     const { id } = req.body;
 
     if (ObjectId.isValid(id)) {
-      const update = await db
+      const deletion = await db
         .collection("students")
         .deleteOne({ _id: ObjectId(id) });
 
-      if (!update) {
+      if (!deletion) {
         throw new Error("Could not delete the selected document.");
       }
 
-      res.json({ message: "Data successfully deleted..." });
+      res.json({ message: "Data successfully deleted from the database..." });
     } else {
       res.status(404).json({ error: "Not a valid document ID." });
     }
