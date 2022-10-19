cda89a2baf0e95a721edd3f921f5e3f781be80ed
wawerusamuel <waweruzamuel@gmail.com>
2022-10-19 14:02:01 +0300

    added an export statement...

--- schemas/authData.js
+++ schemas/authData.js
@@ -4,3 +4,5 @@ const userBodyValidator = Joi.object({
   email: Joi.string().lowercase().required(),
   password: Joi.string().min(8).required().regex(/\w/),
 });
+
+module.exports = userBodyValidator;
