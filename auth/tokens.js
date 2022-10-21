7cfeb83699ce00d6077e5a995390f942f73a5a54
wawerusamuel <waweruzamuel@gmail.com>
2022-10-21 15:07:29 +0300

    added indentation to the token file...

--- auth/tokens.js
+++ auth/tokens.js
@@ -4,14 +4,19 @@ module.exports = {
   signAccessToken: (userId) => {
     return new Promise(async (resolve, reject) => {
       const payload = { userId };
+
       const secret_key = process.env.ACCESS_TOKEN_SECRET;
+
       const options = {
         expiresIn: "2min",
         issuer: "UserDbManager",
         audience: userId,
       };
+
       const token = await jwt.sign(payload, secret_key, options);
+
       if (!token) reject(token);
+
       resolve(token);
     });
   },
